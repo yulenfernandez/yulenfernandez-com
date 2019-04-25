1ddff97ac9ae2dfdd77ccfb5ad0f03a1ed6d1f1a
yulen <17993323+yulenfernandez@users.noreply.github.com>
2019-04-25 22:07:47 +0200

    Draft WIP

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,6 @@
 window.addEventListener('DOMContentLoaded', function() {
 
 	/**
-	 * Manage if content is fixed of use percentage heights.
-	 * Fixed is triggered in small screen were the footer would overlap main content.
-	 */
-	
-	var dLinks = document.getElementById('js-links');
-	var dFooter = document.getElementById('js-footer');
-	var dSiteContent = document.getElementById('js-siteContent');
-
-	if (dFooter.offsetTop - dLinks.offsetTop < 30) {
-		dSiteContent.classList.add('o-site-content--fixed');
-	} else {
-		dSiteContent.classList.remove('o-site-content--fixed');
-	}
-
-
-
-	/**
 	 * Manage pages displays
 	 */
 
