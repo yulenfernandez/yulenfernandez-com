727147688e539cf1c0cbf39ecd82d14da3d0d062
yulen <17993323+yulenfernandez@users.noreply.github.com>
2019-04-27 20:35:59 +0200

    New footer layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,93 +1,111 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', function() {
+/**
+ * Check if content overlap and so add class to modify display  
+ */
+function rwdClassAdd(sElement1, sElement2, sOrientation , iGap, sTargetElement, sClassToAdd) {
+	var dElement1 = document.getElementById(sElement1);
+	var dElement2 = document.getElementById(sElement2);
+	var dTargetElement = document.getElementById(sTargetElement);
 
-	/**
-	 * Manage if content is fixed of use percentage heights.
-	 * Fixed is triggered in small screen were the footer would overlap main content.
-	 */
-	
-	var dLinks = document.getElementById('js-links');
-	var dFooter = document.getElementById('js-footer');
-	var dSiteContent = document.getElementById('js-siteContent');
+	switch (sOrientation) {
+		case 'horizontal':
+			if (dElement2.getBoundingClientRect().left - dElement1.getBoundingClientRect().left < iGap) {
+				dTargetElement.classList.add(sClassToAdd);
+			}
+			break;
+		
+		case 'vertical':
+			if (dElement2.getBoundingClientRect().top - dElement1.getBoundingClientRect().top < iGap) {
+				dTargetElement.classList.add(sClassToAdd);
+			}
+			break;
 
-	if (dFooter.offsetTop - dLinks.offsetTop < 30) {
-		dSiteContent.classList.add('o-site-content--fixed');
-	} else {
-		dSiteContent.classList.remove('o-site-content--fixed');
+		default:
+			console.log('function rwdClassAdd error: bad orientation value, must be "horizontal" or "vertical".');
+			break;
+	}
+}
+
+
+/**
+ * Display the needed page
+ */
+function fnPageHandler() {
+	var dPages = document.querySelectorAll('.js-page');
+	var sTargetPageName = this.getAttribute('targetPage');
+	var dTargetPage = document.querySelector('.js-page[page="' + sTargetPageName + '"]');
+
+	for (var i=0; i != dPages.length; i++) {
+		dPages[i].classList.add('u-display-none');
 	}
 
+	dTargetPage.classList.remove('u-display-none');
+}
 
+
+/**
+ * Empty all inputs (except the input with "submit" type) and textareas
+ */
+function fnClearForm() {
+	var dFields = document.querySelectorAll('input:not([type="submit"]), textarea');
+
+	for(var i=0; i!= dFields.length; i++) {
+		dFields[i].value='';
+	}
+}
+
+
+/**
+ * Manage form submit
+ */
+$("#js-form").submit(function(e) {
+	e.preventDefault();
+
+	var $form = $(this);
+	var formData = $form.serialize();
+	var dFormSubmitBtn = $('#js-formSubmitBtn');
+	var dFormMsg = $('#js-formMsg');
+
+	dFormSubmitBtn.addClass('c-btn--deactivated');
+	dFormMsg.text('Sending your message...');
+	
+	$.ajax({
+		type: 'POST',
+		url: $form.attr('action'),
+		data: formData
+	})
+	.done(function(response) {
+		fnClearForm();
+		dFormSubmitBtn.removeClass('c-btn--deactivated');
+		dFormMsg.removeClass('u-display-none');
+		dFormMsg.text('Your message has been sent.');
+		setTimeout(function() {
+			dFormMsg.addClass('u-display-none');
+		}, 5000);
+	})
+	.fail(function(data) {
+		fnClearForm();
+		dFormSubmitBtn.removeClass('c-btn--deactivated');
+		dFormMsg.removeClass('u-display-none');
+		dFormMsg.text('Sorry, your message couldn\'t be sent.');
+		setTimeout(function() {
+			dFormMsg.addClass('u-display-none');
+		}, 5000);
+	});
+});	
+
+
+window.addEventListener('DOMContentLoaded', function() {
+
+	rwdClassAdd('js-footerLeftSection', 'js-footerRightSection', 'horizontal', 15, 'js-footer', 'c-footer--mobile');
 
 	/**
-	 * Manage pages displays
+	 * Attach to all page links the function to handle the switch
 	 */
-
 	var dPageLinks = document.querySelectorAll('.js-pageLink');
 	for (var i=0; i != dPageLinks.length; i++ ) {
 		dPageLinks[i].addEventListener('click', fnPageHandler, false);	
 	}
 
-	function fnPageHandler() {
-		var dPages = document.querySelectorAll('.js-page');
-		var sTargetPageName = this.getAttribute('targetPage');
-		var dTargetPage = document.querySelector('.js-page[page="' + sTargetPageName + '"]');
-
-		for (var i=0; i != dPages.length; i++) {
-			dPages[i].classList.add('u-display-none');
-		}
-
-		dTargetPage.classList.remove('u-display-none');
-	}
-
-
-
-	/**
-	 * Manage form submit
-	 */
-	function fnClearForm() {
-		var dFields = document.querySelectorAll('input:not([type="submit"]), textarea');
-
-		for(var i=0; i!= dFields.length; i++) {
-			dFields[i].value='';
-		}
-	}
-	
-	$("#js-form").submit(function(e) {
-		e.preventDefault();
-
-		var $form = $(this);
-		var formData = $form.serialize();
-		var dFormSubmitBtn = $('#js-formSubmitBtn');
-		var dFormMsg = $('#js-formMsg');
-
-		dFormSubmitBtn.addClass('c-btn--deactivated');
-		dFormMsg.text('Sending your message...');
-		
-		$.ajax({
-    	type: 'POST',
-    	url: $form.attr('action'),
-    	data: formData
-		})
-		.done(function(response) {
-			fnClearForm();
-			dFormSubmitBtn.removeClass('c-btn--deactivated');
-			dFormMsg.removeClass('u-display-none');
-			dFormMsg.text('Your message has been sent.');
-			setTimeout(function() {
-				dFormMsg.addClass('u-display-none');
-			}, 5000);
-		})
-		.fail(function(data) {
-			fnClearForm();
-			dFormSubmitBtn.removeClass('c-btn--deactivated');
-			dFormMsg.removeClass('u-display-none');
-			dFormMsg.text('Sorry, your message couldn\'t be sent.');
-			setTimeout(function() {
-				dFormMsg.addClass('u-display-none');
-			}, 5000);
-		});
-	});	
-
-
-});
+}, false);
