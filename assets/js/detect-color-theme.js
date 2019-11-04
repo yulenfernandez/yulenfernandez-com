e625344c31ca6496bf46ae0200ba3b1516dfa090
yulen <17993323+yulenfernandez@users.noreply.github.com>
2019-11-04 20:57:05 +0100

    Added js to detect system theme

---
+++ assets/js/detect-color-theme.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//determines if the user has a set theme
+//Source: https://stackoverflow.com/questions/56300132/how-to-over-ride-css-prefers-color-scheme-setting
+function detectColorScheme() {
+	let sTheme = "light"; //default to light
+
+	//local storage is used to override OS theme settings
+	if (localStorage.getItem("theme")) {
+		if (localStorage.getItem("theme") == "dark") {
+			sTheme = "dark";
+		}
+	} else if (!window.matchMedia) {
+		//matchMedia method not supported
+		return false;
+	} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+		//OS theme setting detected as dark
+		sTheme = "dark";
+	}
+
+	//dark theme preferred, set document with a `data-theme` attribute
+	if (sTheme == "dark") {
+		document.documentElement.setAttribute("data-theme", "dark");
+	}
+}
+detectColorScheme();
